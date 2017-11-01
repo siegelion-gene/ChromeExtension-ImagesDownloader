c5a06146fafa26d2b5879069540005a55a50a07f
wangyu <wangyu@wisetv.com.cn>
2017-11-01 15:27:02 +0800

    udpate

--- js/content.js
+++ js/content.js
@@ -1,9 +1,17 @@
+
 chrome.extension.onRequest.addListener(function(links) {
 	for ( var i = 0; i < links.data.length; i++) { 
+		if (!links.data[i] || !(links.data[i].length > 0))
+			continue;
 	    var image = document.createElement("img"); 
 	    var d = document.createElement("div"); 
-	    image.setAttribute("src",links.data[i]); 
+	    image.setAttribute("src",links.data[i]);
+	    var label_div = document.createElement('div');
+	    label_div.setAttribute('class','label')
+	    var label = document.createElement('p');
+	    label_div.appendChild(label)
 	    d.appendChild(image);
+	    d.appendChild(label_div);
 	    d.setAttribute("class",'item');
 	    document.getElementById("content").appendChild(d) 
 	}
