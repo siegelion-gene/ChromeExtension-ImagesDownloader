de3b87502475f63b4e2c48fad0e26e9cc76d4d49
siegelion-gene <32829878+siegelion-gene@users.noreply.github.com>
2020-04-24 17:31:45 +0800

    Add files via upload

--- content-script.js
+++ content-script.js
@@ -1,20 +1,50 @@
+var host = window.location.host
+var url = window.location.toString()
+var image_repl_src = "https://www.lobotz.com/wp-content/uploads/edd/2018/03/SL3.jpg"
+
 document.addEventListener('DOMContentLoaded', function()
 {
-    function nodeModify(obj){
-        obj.innerText="hi"//+obj.innerText
+    function nodeModify(node){
+        console.log("nodeModify")
+        console.log(node)
+        if(node.tagName == "UL"){
+            $(node).find('img.mimg').each(function(index, img){
+                $(img).attr("src", image_repl_src)
+            })
+        }
+            //obj.innerText="hi"//+obj.innerText
     }
-    var observe=new MutationObserver(function (mutations,observe) {
-        m = $(mutations)
-        m.each(function(){
-            $(this.addedNodes).each(function(){nodeModify(this)})
 
-            console.log(this)
-            console.log(this.addedNodes)
-        });
+    function repl(){
+        console.log(host)
+        if(host.match(".*\.bing\.com")){
+            repl_bing()
+        }
+    }
+
+    function repl_bing(){
+        $(document).find('img.mimg').each(function(img){
+            $(img).attr("src", image_repl_src)
+        })
+    }
+	
+    var observer=new MutationObserver(function (mutations,observe) {
+//        m = $(mutations)
+//        repl()
+//        console.log(m)
+        mutations.forEach(function(mutation){
+//            console.log("each mutation")
+            if(target.isEqualNode(mutation.target)){
+                console.log(mutation)
+                mutation.addedNodes.forEach(function(node){
+                    nodeModify(node)})
+            }
+         });
     });
     var target = document.getElementById('mmComponent_images_1')
+    var config = {childList: true, subtree: false, attributeFilter: [ "li" ]}
     if(target){
-        observe.observe(target,{childList: true, subtree: true});
+        observer.observe(target, config);
     }
 })
 
